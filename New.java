b66533f896e40f6e7fbb1e85aa21c7b6f35bb46c
Ubiin <byi1997@naver.com>
2017-11-03 10:16:42 +0900

    ㄴㅇㄹ
    ㄴㄹ

--- New.java
+++ New.java
@@ -4,12 +4,7 @@ public class New {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello2!");
-<<<<<<< HEAD
-		System.out.println("안녕하세요");
-=======
-		System.out.println("안녕하세요22222");
->>>>>>> yubin
+		System.out.println("hello!-last");
 
 	}
 
