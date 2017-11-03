150c945cacc1708e211a81214771448fb029bf62
Ubiin <byi1997@naver.com>
2017-11-03 10:28:20 +0900

    a
    sf

--- New.java
+++ New.java
@@ -4,15 +4,8 @@ public class New {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("hello!-last");
-=======
 		System.out.println("hello!-last22222");
-<<<<<<< HEAD
->>>>>>> yubin
-=======
 		System.out.println("hello!-last33333");
->>>>>>> yubin
 
 	}
 
